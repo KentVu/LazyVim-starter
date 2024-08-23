64f7cb52f400378a70b76053d517c917b534b5a7
Vu Tran Kien <vutrankien.vn@gmail.com>
2024-08-23 18:18:23 +0900

    LazyVim v10, ideals open AS, .ideavimrc
    Use classic surround binding. (cs, ys,..)
    Use default vue-ls.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+--vim.api.nvim_create_autocmd("LspAttach", {
+--  callback = function(args)
+--    local bufnr = args.buf
+--    local client = vim.lsp.get_client_by_id(args.data.client_id)
+--    if client.name == "ideals" then
+--    end
+--  end,
+--})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.kt{s,}", "*.java", "*.gradle*" },
+  callback = function(args)
+    --vim.cmd('checktime')
+    --[[vim.cmd(
+    --  [[exec "!'/Applications/Android Studio.app/Contents/MacOS/studio' --line ".line('.')." --column ".(col('.')-1)." %:p"]]
+    --)]]
+  end,
+})
