d689a26136e5aeba50e95d9b47ef4dd705e4955d
Vu Tran Kien <vutrankien.vn@gmail.com>
2025-07-07 11:04:17 +0900

    FIX lsp as nvim-0.11 update (lspconfig changes)
    Add zk-nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,10 +2,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
-    -- init = function() end,
+    init = function()
+    end,
     -- see also `:h lspconfig-new`, https://www.lazyvim.org/configuration/examples
     ---@class PluginLspOpts
     opts = function(_, opts)
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      -- lspconfig.ideals.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- table.insert(opts, value)
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+          "documentation",
+          "detail",
+          "additionalTextEdits",
+        },
+      }
       local Keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- stylua: ignore
       vim.list_extend(Keys, {
@@ -17,23 +31,18 @@ return {
       })
 
       -- thanks https://github.com/SuduIDE/ideals/issues/59#issuecomment-1348761041
-      local lspconfig = require("lspconfig")
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-      local configs = require("lspconfig.configs")
-      local util = require("lspconfig.util")
+      local lspconfig = require("lspconfig") --vim.lsp
+      local configs = require("lspconfig.configs") --vim.lsp.configs
+      local util = require("lspconfig.util") --vim.lsp.util
 
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-      capabilities.textDocument.completion.completionItem.resolveSupport = {
-        properties = {
-          "documentation",
-          "detail",
-          "additionalTextEdits",
-        },
-      }
-
-      configs.ideals = {
+      --https://neovim.io/doc/user/lsp.html
+      --vim.lsp.config['ideals'] = {
+      vim.lsp.config('ideals', {
+      --configs.ideals = {
         default_config = {
+        },
           --cmd = { "/Applications/IntelliJ IDEA CE.app/Contents/MacOS/idea", "lsp-server" },
+        -- TODO: check tcp-way https://github.com/Kotlin/kotlin-lsp/blob/main/scripts/neovim.md#tcp-way
           cmd = { "nc", "localhost", "8989" },
           filetypes = {
             --"ideals", --Start by LspStart manually.
@@ -42,42 +51,43 @@ return {
             "groovy",
             "java",
           },
-          root_dir = function(pattern)
-            local cwd = vim.loop.cwd()
+          --[[root_dir = function(bufnr, on_dir)
+            local cwd = vim.fn.getcwd() --vim.loop.cwd()
             local root = util.root_pattern(".idea")(pattern)
-            return util.path.is_descendant(cwd, root) and cwd or root
-          end,
+          on_dir(util.root_pattern('.idea', 'build.gradle.kts')(fname))
+            --return util.path.is_descendant(cwd, root) and cwd or root
+          end,]]
+        root_markers= { "build.gradle*", ".idea"},
           autostart = false,
-        },
-        --on_attach = function(client, bufnr) end,
-      }
-
-      -- lspconfig.ideals.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- table.insert(opts, value)
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>i", group = "ideals", mode = { "n", "v" } },
-      })
-      opts.servers.ideals = {
         capabilities = capabilities,
-        on_attach = function()
+        on_attach = function(client, bufnr)
           vim.o.autowrite = false
+          -- Auto-formatting can be disabled with:
+          vim.b.autoformat = false -- buffer-local
+          local wk = require("which-key")
+          wk.add({
+            { "<leader>i", group = "ideals", mode = { "n", "v" } },
+          })
           vim.keymap.set(
             "n",
             "<leader>ii",
-            [[<cmd>exec "!'/opt/android-studio/bin/studio.sh' --line ".line('.')." --column ".(col('.')-1)." %:p"<CR>]],
+            [[<cmd>exec "!'/Applications/Android Studio.app/Contents/MacOS/studio' --line ".line('.')." --column ".(col('.')-1)." %:p"<CR>]],
             { buffer = true, noremap = true, desc = "Open in A.Studio" }
           )
           vim.keymap.set(
             "n",
             "<leader>is",
-            [[<cmd>!DISPLAY=:1 xdotool key ctrl+s; sleep 0.5<CR>]],
+            [[<cmd>!osascript -e 'tell application "System Events" to keystroke "s" using {command down}'; sleep 0.3<CR>]],
             { buffer = true, noremap = true, desc = "Save in A.Studio" }
           )
         end,
-      }
+      })
+      --vim.lsp.enable('ideals') --no autostart!!
+
+      --[[opts.servers.ideals = {
+        on_attach = function()
+        end,
+      }]]
       --opts.setup.ideals = function(server, opts)
       --  -- return true if you don't want this server to be setup with lspconfig
       --  return false
